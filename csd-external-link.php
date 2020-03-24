b8031a89bbe6d1472792401b52a67ecf7c0ed69f
Josh Armentano <josh@abidewebdesign.com>
2020-03-24 09:10:43 -0700

    Renaming

--- csd-external-link.php
+++ csd-external-link.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: CSD Functions - External Links
+Plugin Name: CSD Functions - External Link
 Version: 1.0
 Description: Display pop-up message when leaving district websites.
 Author: Josh Armentano
@@ -9,7 +9,7 @@ Plugin URI: https://abidewebdesign.com
 */
 require WP_CONTENT_DIR . '/plugins/plugin-update-checker-master/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://github.com/csd509j/csd-functions-external-links',
+	'https://github.com/AbideWebDesign/csd-functions-external-link',
 	__FILE__,
 	'csd-functions-external-links'
 );
@@ -22,7 +22,7 @@ function external_link_enqueue_script() {
 	
 	$plugin = get_plugin_data( __FILE__, false, false );
 	
-	wp_enqueue_script( 'external-link.js', plugin_dir_url( __FILE__ ) . '/assets/js/csd-external-links-min.js', '', '', true );
+	wp_enqueue_script( 'external-link.js', plugin_dir_url( __FILE__ ) . '/assets/js/csd-external-link-min.js', '', '', true );
 	
 }
 
